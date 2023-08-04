f41cb6b0ca57bcd387a7442ee1bdc4de300154ac
Mreselassie001 <mreselassie1@gmail.com>
2023-08-04 16:02:02 +0100

    04/08/23 3:02

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,53 +1,56 @@
 import React from "react";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 
 const Frontend = () => {
   return (
     <div className="skills__content">
       <div className="skills__data">
-        {/* <i className="bx bx-badge-check"></i> */}
-
         <div>
-        <Icon className="frontend-icons"  icon="icon-park-outline:html-five" /> 
-        
+          <Icon className="frontend-icons" icon="icon-park-outline:html-five" />
           <h3 className="skills__name">HTML</h3>
           <span className="skills__level">Advanced</span>
         </div>
       </div>
-      <div className="skills__data">
-      
 
+      <div className="skills__data">
         <div>
-        <Icon className="frontend-icons" icon="el:css" />
+          <Icon className="frontend-icons" icon="el:css" />
           <h3 className="skills__name">CSS</h3>
           <span className="skills__level">Advanced</span>
         </div>
       </div>
-      <div className="skills__data">
-        {/* <i className="bx bx-badge-check"></i> */}
 
+      <div className="skills__data">
         <div>
-        <Icon className="frontend-icons" icon="fa-brands:js-square" />
+          <Icon className="frontend-icons" icon="fa-brands:js-square" />
           <h3 className="skills__name">JavaScript</h3>
           <span className="skills__level">Basic</span>
         </div>
       </div>
 
       <div className="skills__data">
-        {/* <i className="bx bx-badge-check"></i> */}
-
         <div>
-        <Icon className="frontend-icons" icon="nonicons:react-16" />
+          <Icon
+            className="frontend-icons"
+            icon="akar-icons:nextjs-fill"
+            width="70"
+          />
+          <h3 className="skills__name">Nextjs</h3>
+          <span className="skills__level">Basic</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+        <div>
+          <Icon className="frontend-icons" icon="nonicons:react-16" />
           <h3 className="skills__name">React</h3>
           <span className="skills__level">Intermediate</span>
         </div>
       </div>
 
       <div className="skills__data">
-        {/* <i className="bx bx-badge-check"></i> */}
-
         <div>
-        <Icon className="frontend-icons" icon="akar-icons:figma-fill" />
+          <Icon className="frontend-icons" icon="akar-icons:figma-fill" />
           <h3 className="skills__name">Figma</h3>
           <span className="skills__level">Basic</span>
         </div>
