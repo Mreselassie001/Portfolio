bbb1de88c6bb9d5aafc05f0fe36460bf09307ec6
Mreselassie001 <mreselassie1@gmail.com>
2023-08-05 01:49:01 +0100

    05/08/23 12:48am

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -5,31 +5,31 @@ const Frontend = () => {
   return (
     <div className="skills__content">
       <div className="skills__data">
-        <div>
+        <div className="skill__view">
           <Icon className="frontend-icons" icon="icon-park-outline:html-five" />
           <h3 className="skills__name">HTML</h3>
           <span className="skills__level">Advanced</span>
-        </div>
+        </div >
       </div>
 
       <div className="skills__data">
-        <div>
+        <div className="skill__view">
           <Icon className="frontend-icons" icon="el:css" />
           <h3 className="skills__name">CSS</h3>
           <span className="skills__level">Advanced</span>
-        </div>
+        </div >
       </div>
 
       <div className="skills__data">
-        <div>
+        <div className="skill__view">
           <Icon className="frontend-icons" icon="fa-brands:js-square" />
           <h3 className="skills__name">JavaScript</h3>
           <span className="skills__level">Basic</span>
-        </div>
+        </div >
       </div>
 
       <div className="skills__data">
-        <div>
+        <div className="skill__view">
           <Icon
             className="frontend-icons"
             icon="akar-icons:nextjs-fill"
@@ -37,23 +37,23 @@ const Frontend = () => {
           />
           <h3 className="skills__name">Nextjs</h3>
           <span className="skills__level">Basic</span>
-        </div>
+        </div >
       </div>
 
       <div className="skills__data">
-        <div>
+        <div className="skill__view">
           <Icon className="frontend-icons" icon="nonicons:react-16" />
           <h3 className="skills__name">React</h3>
           <span className="skills__level">Intermediate</span>
-        </div>
+        </div >
       </div>
 
       <div className="skills__data">
-        <div>
+        <div className="skill__view">
           <Icon className="frontend-icons" icon="akar-icons:figma-fill" />
           <h3 className="skills__name">Figma</h3>
           <span className="skills__level">Basic</span>
-        </div>
+        </div >
       </div>
 
       {/* </div> */}
