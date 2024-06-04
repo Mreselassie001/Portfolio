cb3c678629561084fc0b89525febc05ed8d4b7fc
Mreselassie001 <mreselassie1@gmail.com>
2024-06-04 18:44:15 +0100

    last commit hopefullyish1.3

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,9 +1,9 @@
 import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
-// import Work4 from "../../assets/work4.jpg";
+import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
-// import Work6 from "../../assets/work6.jpg";
+import Work6 from "../../assets/work6.jpg";
 import Work7 from "../../assets/work7.jpg";
 import Work8 from "../../assets/work8.jpg";
 
@@ -32,13 +32,13 @@ export const projectsData = [
     link: "https://weather-6si6y2hnj-mreselassie1.vercel.app/",
     type: "JavaScript",
   },
-  // {
-  //   id: 4,
-  //   image: Work4,
-  //   title: "",
-  //   category: "app",
-  //   type: "Coming Soon...",
-  // },
+  {
+    id: 4,
+    image: Work4,
+    title: "",
+    category: "app",
+    type: "Coming Soon...",
+  },
   {
     id: 5,
     image: Work5,
@@ -47,28 +47,28 @@ export const projectsData = [
     type: "HTML & CSS",
     link: "https://voluble-tarsier-52a7ee.netlify.app/",
   },
-  // {
-  //   id: 6,
-  //   image: Work6,
-  //   title: "",
-  //   category: "Web",
-  //   type: "Coming Soon...",
-  // },
+  {
+    id: 6,
+    image: Work6,
+    title: "",
+    category: "Web",
+    type: "Coming Soon...",
+  },
   {
     id: 7,
     image: Work7,
     title: "Web Design",
     category: "Web",
     type: "React",
-    link:"https://razorpro9981.github.io/something/",
+    link: "https://razorpro9981.github.io/something/",
   },
-  // {
-  //   id: 8,
-  //   image: Work8,
-  //   title: "",
-  //   category: "Web",
-  //   type: "Coming Soon...",
-  // },
+  {
+    id: 8,
+    image: Work8,
+    title: "",
+    category: "Web",
+    type: "Coming Soon...",
+  },
 ];
 
 export const projectsNav = [
